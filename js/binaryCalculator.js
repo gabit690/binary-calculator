6ed82b5e1a4f19275898a3dcb9bb900bc4dcdff1
gabit <gabit690@gmail.com>
2021-01-30 12:55:06 -0300

    Funcionalidad de los botones para realizar operaciones

--- js/binaryCalculator.js
+++ js/binaryCalculator.js
@@ -6,9 +6,6 @@ const calculator = {
   operand2: "",
   binary: (number) => parseInt((number >>> 0).toString(2)),
   decimal: (number) => parseInt(number, 2),
-  getOperator: function () { 
-    return this.operator 
-  },
   existResult: function () {
     return this.result;
   },
@@ -22,32 +19,61 @@ const calculator = {
       this.screen.innerText += "1";
     }
   },
-  inputOperator: function(operator) {
-    this.operator = operator;
-    this.screen.innerText += operator;
+  setOperator: function(operator) {
+
+    if (this.operator == "" && /[01]$/.test(this.screen.innerText)) {
+      this.operator = operator;
+      this.screen.innerText += operator;
+    } else if (/[+\-*\/]$/.test(this.screen.innerText)) {
+      this.operator = operator;
+      const newExpression = this.screen.innerText.slice(0, this.screen.innerText.length - 1);
+      this.screen.innerText = newExpression + operator;
+    }
+
+    if (this.result && /[01]+/.test(this.screen.innerText)) {
+      this.result = false;
+    }
+
   },
   makeAddition: function() {
-    this.screen.innerText = binary(decimal(this.operand1) + decimal(this.operand2));
+    this.screen.innerText = this.binary(this.decimal(this.operand1) + this.decimal(this.operand2));
     this.result = true;
     this.resetExpression();
   },
   makeSubtraction: function() {
-    this.screen.innerText = binary(decimal(this.operand1) - decimal(this.operand2));
+    this.screen.innerText = this.binary(this.decimal(this.operand1) - this.decimal(this.operand2));
     this.result = true;
     this.resetExpression();
   },
   makeMultiplication : function() {
-    this.screen.innerText =  binary(decimal(this.operand1) * decimal(this.operand2));
+    this.screen.innerText =  this.binary(this.decimal(this.operand1) * this.decimal(this.operand2));
     this.result = true;
     this.resetExpression();
   },
   makeDivision: function() {
-    if (b != 0) {
-        this.screen.innerText = binary(parseInt(decimal(this.operand1) / decimal(this.operand2)));
+    if (this.operand2 != 0) {
+        this.screen.innerText = this.binary(parseInt(this.decimal(this.operand1) / this.decimal(this.operand2)));
     } else {
         this.screen.innerText = "Zero division error!";
     }
+    this.result = true;
     this.resetExpression();
+  },
+  makeOperation: function() {
+
+    this.operand1 = this.screen.innerText.match(/^[01]+/)[0];
+    this.operand2 = this.screen.innerText.match(/[01]+$/)[0];
+
+    switch(this.operator) {
+      case "+": this.makeAddition();
+                break;
+      case "-": this.makeSubtraction();
+                break;
+      case "*": this.makeMultiplication();
+                break;
+      case "/": this.makeDivision();
+                break;
+    }
   },
   clear: function() {
     this.screen.innerText = "";
@@ -55,7 +81,8 @@ const calculator = {
     this.resetExpression();
   },
   validExpression: function() {
-    return (this.operand1 != "" && this.operator != "" && this.operand2 != "");
+    const expectedExpression = /^[01]+[+\-*\/][01]+$/;
+    return (expectedExpression.test(this.screen.innerText));
   },
   resetExpression: function() {
     this.operand1 = "";
@@ -82,42 +109,24 @@ document.getElementById("btn1").addEventListener("click", () => {
   }
 });
 
-
-
-
-
-
 document.getElementById("btnSum").addEventListener("click", () => {
-  calculator.inputOperator("+");
+  calculator.setOperator("+");
 });
 
 document.getElementById("btnSub").addEventListener("click", () => {
-  calculator.inputOperator("-");
+  calculator.setOperator("-");
 });
 
 document.getElementById("btnMul").addEventListener("click", () => {
-  calculator.inputOperator("*");
+  calculator.setOperator("*");
 });
 
 document.getElementById("btnDiv").addEventListener("click", () => {
-  calculator.inputOperator("/");
+  calculator.setOperator("/");
 });
 
 document.getElementById("btnEql").addEventListener("click", () => {
-  
   if (calculator.validExpression()) {
-    
-    switch(calculator.getOperator()) {
-      case "+": calculator.makeAddition();
-                break;
-      case "-": calculator.makeSubtraction();
-                break;
-      case "*": calculator.makeMultiplication();
-                break;
-      case "/": calculator.makeDivision();
-                break;
-    }
-    
+    calculator.makeOperation();
   }
-
 });
